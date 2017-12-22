a74ea93fc741e649fd70911700456f295411fcd0
Adam Skinner <adam.skinner@gmail.com>
2017-12-22 17:38:51 -0500

    package.json fleshed out, eslintrc fleshed out
    I had some trouble actually getting eslint to give compact results, so I went to the standard stylish (I really only did compact because I thought I'd have more success with parsing).

--- template.js
+++ template.js
@@ -1,4 +1,42 @@
-if (1 == -0) derp;;
-if (typeof "abc" == '\derp') herp;
-var x = 1; delete x;
+import React from 'react';
+import { Animated, Dimensions } from 'react-native';
+import { Scene } from 'react-native-router-flux';
+import * as sceneKeys from './sceneKeys';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
+import CTabBar from 'components/general/CTabBar';
 
+const syntaxErrors = (props) => {
+  if (1 == -0) derp;;
+ 
+  if (typeof "abc" == '\derp') herp;
+   
+  var x = 1; delete x;
+};
